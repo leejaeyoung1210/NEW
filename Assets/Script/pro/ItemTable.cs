b961bcb2835cdd27e8c5ce23b4a9a1fab78eb972
leejaeyoung <kltop1210@gmail.com>
2025-09-03 17:30:28 +0900

    에셋다운 및 기타등등

---
+++ Assets/Script/pro/ItemTable.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class ItemTable : DataTable
+{
+    public override void Load(string filename)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public class ItemData
+    {
+        public string Name;
+        public string Icon;
+        public int Num1;
+        public int Num2;
+        public string String;
+    }
+
+
+
+}
